384de8dc7421293779ad3c73633da775898da3db
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2025-01-08 15:35:28 +0100

    ADD middlewares and Cors

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const movieRouter = require('./routers/movieRouter')
 const errorsHandler = require('./middlewares/errorsHandler')
 const notFound = require('./middlewares/notFound')
 
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN
+    })
+)
+
 app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
     res.send('Server is running')
