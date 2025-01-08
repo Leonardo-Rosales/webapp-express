63313328576435a04c8c03fc0774ff242e846e63
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2025-01-08 14:27:14 +0100

    ADD index and show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,12 +1,52 @@
 const connection = require('../data/db')
 
 function index(req, res) {
-    res.json('index');
+
+    const sql = `
+    SELECT * 
+    FROM movies`
+
+    connection.query(sql, (err, movies) => {
+
+        if (err) return res.status(500).json({ message: err.message })
+
+        res.json(movies)
+    })
 
 }
 
 function show(req, res) {
-    res.json('show');
+
+    const id = (req.params.id)
+
+    const sql = `
+    SELECT *
+    FROM movies
+    WHERE id = ?`
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ message: err.message })
+        if (results.length === 0)
+            return res.status(404).json({
+                error: 'Not found',
+                message: 'Movie not found'
+            })
+
+        const movie = results[0]
+
+        const sql = `
+        SELECT *
+        FROM reviews
+        WHERE movie_id = ?`
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message })
+
+            movie.reviews = results
+
+            res.json(movie)
+        })
+    })
 
 }
 
